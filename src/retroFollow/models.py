dc46f0b0503557f30dcddace6ca3c46ece280c64
veatch <jtveatch@gmail.com>
2011-01-24 22:26:40 -0500

    cleaned up fetch_first_page a bit

--- src/retroFollow/models.py
+++ src/retroFollow/models.py
@@ -3,7 +3,7 @@ import tweepy
 from django.db import models
 
 from settings import tweets_per_page
-    
+
 class UserTwitter(models.Model):
     '''
     #class Meta:
@@ -34,7 +34,7 @@ class UserTwitter(models.Model):
                 print locale.format('%d', search_id, True)
             else:
                 max_id = search_id
-                search_id = search_id - (max_id - low_id)/2 
+                search_id = search_id - (max_id - low_id)/2
                 print 'HIGH'
                 print locale.format('%d', search_id, True)
         # trying to get first tweet at beginning so we can grab first tweet time
@@ -81,44 +81,39 @@ class Manager(models.Model):
         if tweets == 20:
             pass
         return tweets
-        
 
-    #                                       REFACTOR THIS SHIT
     def fetch_first_page(self):
         print 'fetching first page...'
         tweets_per_user = 3200
-        max_page = 3200 / 20
+        max_page = tweets_per_user / tweets_per_page
 
-        tweets = Tweet.objects.filter(user=self.user).order_by('created_at')[:tweets_per_page]
-        if len(tweets) == tweets_per_page:
-            return tweets
+        if Tweet.objects.filter(user=self.user).count() < tweets_per_page:
+            api = tweepy.API()
+            api_user = api.get_user(self.user.username)
+            user_tweet_count = api_user.statuses_count
+            #make sure this only happens at creation
+            if user_tweet_count > tweets_per_user:
+                self.user.old_timer_and_or_gabber=True
+                self.user.save()
 
-        api = tweepy.API()
-        api_user = api.get_user(self.user.username)
-        user_tweet_count = api_user.statuses_count
-        #make sure this only happens at creation
-        if user_tweet_count > tweets_per_user:
-            self.user.old_timer_and_or_gabber=True
-            self.user.save()
+    	    #except (TweepError):
+    	    #    return render_to_response('user_timeline.html', {'error_message':'shit'})
+    	    # also show join date
 
-	    #except (TweepError):
-	    #    return render_to_response('user_timeline.html', {'error_message':'shit'})
-	    # also show join date
-        first_page_num = user_tweet_count / tweets_per_page
-        if first_page_num > max_page:
-            first_page_num = max_page
-        self.fetch_twitter_page(first_page_num)
+            first_page_num = user_tweet_count / tweets_per_page
+            if first_page_num > max_page:
+                first_page_num = max_page
+            if first_page_num > 1:
+                self.fetch_twitter_page(first_page_num-1)
+            self.fetch_twitter_page(first_page_num)
+            # just a doublecheck for earlier tweets? not sure what this is about
+            if user_tweet_count % tweets_per_page != 0 and first_page_num != max_page:
+           	    self.fetch_twitter_page(first_page_num+1)
 
-        if user_tweet_count % tweets_per_page != 0 and first_page_num != max_page:
-            first_page_num = first_page_num + 1
-       	    self.fetch_twitter_page(first_page_num)
-
-        tweets = Tweet.objects.filter(user=self.user).order_by('created_at')[:tweets_per_page]
-        if len(tweets) == tweets_per_page:
-            return tweets_per_page
-
-        tweets = self.search_for_first_tweets(min_page=0, max_page=first_page_num)
-        for tweet in tweets:
-            Tweet.objects.get_or_create(user=self.user, text=tweet.text, created_at=tweet.created_at, tweet_id=tweet.id)
+            # if twitter's page count is off, search for first page
+            if Tweet.objects.filter(user=self.user).count() < tweets_per_page:
+                tweets = self.search_for_first_tweets(min_page=0, max_page=first_page_num)
+                for tweet in tweets:
+                    Tweet.objects.get_or_create(user=self.user, text=tweet.text, created_at=tweet.created_at, tweet_id=tweet.id)
 
         return Tweet.objects.filter(user=self.user).order_by('created_at')[:tweets_per_page]
