c90c9b0c247ee2324ca56d13665c42e86cd5eb38
veatch <jtveatch@gmail.com>
2011-03-27 22:14:17 -0400

    Link to twitter from single-tweet view. Link to previous page on user timeline.

--- src/retroFollow/views.py
+++ src/retroFollow/views.py
@@ -10,12 +10,14 @@ def user_timeline(request, username, page_num=1):
     if username == 'favicon.ico':
         return None
     # if user is protected, offer oauth
+    # take a look at IE before shipping
 
     user, tweets = fetch_page(username, page_num) #also fetch if user !created, but tweets not in db
     #  boolean for old tweets not available instead of passing user around
+    # end of feed
     return render_to_response('user_timeline.html',
-                             {'tweets':tweets, 'user':user, 'username':username, 'next_page':int(page_num)+1,},#detect when there are no more pages
-                             context_instance=RequestContext(request),)# prev page
+                             {'tweets':tweets, 'user':user, 'username':username, 'prev_page':int(page_num)-1, 'next_page':int(page_num)+1,},#detect when there are no more pages
+                             context_instance=RequestContext(request),)
 
 def single_tweet(request, username, tweet_id):
     # ignore favicon requests
